de5e47e4091af3a47977d9ff8cf8042676ad8384
Ivan Ilak <ivan.ilak@hotmail.com>
2025-03-13 23:10:20 +0100

    Add debug prints

--- src/util.rs
+++ src/util.rs
@@ -25,7 +25,7 @@ pub fn copy_dir_all(src: &Path, dst: &Path) {
     if !dst.exists() {
         fs::create_dir_all(dst).unwrap_or_else(|_| panic!("Could not create folder:\t {:?}", dst));
     }
-    println!({:#?}, src);
+    println!("{}", src.display());
     for entry in fs::read_dir(src).expect("Could not read source director!") {
         let entry = entry.expect("Problem parsing file!");
         let entry_path = entry.path();
